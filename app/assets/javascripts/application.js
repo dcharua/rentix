9f672d765c6c8dfbda7171e42c8e868ba4944d1a
dcharua <danielcharua@hotmail.com>
2017-10-13 19:54:27 -0500

    pg-22

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -60,10 +60,8 @@
 //= require jvectormap/jquery-jvectormap-2.0.2.min.js
 //= require jvectormap/jquery-jvectormap-world-mill-en.js
 //= require toastr/toastr.min.js
-<<<<<<< HEAD
 //= require sweetalert/sweetalert.min.js
-=======
->>>>>>> 70f58b6cee663fd805fc7b765c9c26881260a93b
+
 
 var hide_spinner = function(){
   $('#spinner').hide();
